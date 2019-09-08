3b486915e63aa231ec42ec04886f6a2040dcc005
Renato <ramos.renato@outlook.com>
2019-09-08 15:05:11 -0300

    first commit

---
+++ two-fer/TwoFer.fs
@@ -0,0 +1,7 @@
+﻿module TwoFer
+open System
+
+let twoFer (input: string option): string =
+    if input.IsSome
+    then sprintf "One for %s, one for me." input.Value
+    else "One for you, one for me."
