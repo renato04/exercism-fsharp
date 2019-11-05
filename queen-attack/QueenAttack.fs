6af2a703159455d78d71f6c5cf15b1d7780176f5
RENATO RAMOS NASCIMENTO <rnascimento@corp.br>
2019-11-05 13:55:25 -0300

    fixing queen attack solution

--- queen-attack/QueenAttack.fs
+++ queen-attack/QueenAttack.fs
@@ -1,9 +1,8 @@
 ﻿module QueenAttack
 
 let create (position: int * int) = 
-    match position with
-    |(x, y) when (x < 0 || y < 0)  || (x > 7 || y > 7) -> false
-    |(_, _) -> true
+    let x, y = position
+    not ((x < 0 || y < 0)  || (x > 7 || y > 7))
 
 let isOnTheSameRow (queen1: int * int) (queen2: int * int) =
     let queen1Row = fst queen1
@@ -17,55 +16,27 @@ let isOnTheSameColumn (queen1: int * int) (queen2: int * int) =
 
     queen1Row = queen2Row
 
-let isOnUpperLeftDiagonal (queen1: int * int) (queen2: int * int) =
+let isOnDiagonal (queen1: int * int) (queen2: int * int) =
     let queen1Row, queen1Column = queen1
-
-    let diagonal = [
+    [
         for i in [1 .. 7] ->
             ((queen1Row - i), (queen1Column - i))
     ]
-
-    diagonal
-    |> List.contains queen2
-
-let isOnUpperRightDiagonal (queen1: int * int) (queen2: int * int) =
-    let queen1Row, queen1Column = queen1
-
-    let diagonal = [
+    |> List.append [
         for i in [1 .. 7] ->
             ((queen1Row - i), (queen1Column + i))
     ]
-
-    diagonal
-    |> List.contains queen2
-
-let isOnDownLeftDiagonal (queen1: int * int) (queen2: int * int) =
-    let queen1Row, queen1Column = queen1
-
-    let diagonal = [
+    |> List.append  [
         for i in [1 .. 7] ->
             ((queen1Row + i), (queen1Column - i))
     ]
-
-    diagonal
-    |> List.contains queen2
-
-let isOnDownRightDiagonal (queen1: int * int) (queen2: int * int) =
-    let queen1Row, queen1Column = queen1
-
-    let diagonal = [
+    |> List.append [
         for i in [1 .. 7] ->
             ((queen1Row + i), (queen1Column + i))
     ]
-
-    diagonal
     |> List.contains queen2
-
-let isOnTheSameDiagonal (queen1: int * int) (queen2: int * int) = 
-    (isOnUpperLeftDiagonal queen1 queen2) || (isOnUpperRightDiagonal queen1 queen2) ||
-    (isOnDownLeftDiagonal queen1 queen2) || (isOnDownRightDiagonal queen1 queen2)
 
 let canAttack (queen1: int * int) (queen2: int * int) = 
     (isOnTheSameColumn queen1 queen2) || (isOnTheSameRow queen1 queen2) ||
-    (isOnTheSameDiagonal queen1 queen2)
+    (isOnDiagonal queen1 queen2)
     
