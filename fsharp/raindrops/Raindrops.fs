29643211d315cc6df4dc6a2f1c40bfa5e4defd59
Renato Ramos Nascimento <rnascimento@sompo.com.br>
2019-11-26 18:24:35 -0300

    adding rain drop solution

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,16 @@
+﻿module Raindrops
+
+let factors = Map.empty.
+                Add(3, "Pling").
+                Add(5, "Plang").
+                Add(7, "Plong")
+
+let convert (number: int): string = 
+    factors
+    |> Map.filter (fun factor _ -> number % factor = 0)
+    |> (fun factors -> 
+            if Map.isEmpty factors then 
+                number.ToString()
+            else
+                Map.fold (fun state _ value -> state + value) "" factors
+       ) 
---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,7 @@
+﻿module TwoFer
+open System
+
+let twoFer (input: string option): string =
+    input
+    |> Option.defaultValue "you" input
+    |> sprintf "One for %s, one for me."
