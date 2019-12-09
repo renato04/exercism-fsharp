964b444380a3a725fb9ef50026f8aea0b63a0b3f
Renato Ramos Nascimento <ramos.renato@outlook.com>
2019-12-09 13:41:46 -0300

    Adding WordCount Solution

---
+++ fsharp/word-count/WordCount.fs
@@ -0,0 +1,12 @@
+﻿module WordCount
+open System.Text.RegularExpressions
+
+let countWords (phrase: string) =
+    let getValidValues (value: string) =
+        Regex.Matches(value.ToLowerInvariant(), @"\b[\w']+\b")
+    
+    phrase
+    |> getValidValues
+    |> Seq.map (fun x -> x.Value)
+    |> Seq.countBy id
+    |> Map.ofSeq
