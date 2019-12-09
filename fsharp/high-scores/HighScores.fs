f2cb854779fa223cd50815d4a65811cc0f2bb36f
Renato Ramos Nascimento <ramos.renato@outlook.com>
2019-12-09 10:21:06 -0300

    Adding HighScores Solution

---
+++ fsharp/high-scores/HighScores.fs
@@ -0,0 +1,25 @@
+﻿module HighScores
+
+let scores (values: int list): int list = 
+    values
+
+let latest (values: int list): int = 
+    values
+    |> List.filter (fun value -> value > 0)
+    |> List.min
+
+let personalBest (values: int list): int = 
+    values
+    |> List.max
+
+let personalTopThree (values: int list): int list = 
+    values
+    |> List.sortDescending
+    |> (fun l -> 
+        if List.length l < 3 then
+            l
+        else
+            l
+            |> List.take  3
+    )
+    
