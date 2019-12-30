3bc02b50655595f3d09a83fcf0eac528cd686446
Renato Ramos Nascimento <ramos.renato@outlook.com>
2019-12-30 16:18:13 -0300

    Adding another solution

---
+++ fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -0,0 +1,28 @@
+﻿module KindergartenGarden
+
+// TODO: define the Plant type
+type Plant =
+    | Grass
+    | Clover
+    | Radishes
+    | Violets
+
+let charToPlant char =
+    match char with
+    | 'C' -> Clover
+    | 'G' -> Grass
+    | 'R' -> Radishes
+    | _   -> Violets
+
+
+let plants (diagram: string) (student: string) = 
+    let diagrams = diagram.ToUpper().Split('\n')
+    let position = (int (student.[0]) - int 'A') * 2
+    
+    [ 
+        diagrams.[0].[position]; 
+        diagrams.[0].[position + 1]; 
+        diagrams.[1].[position]; 
+        diagrams.[1].[position + 1]
+    ] 
+    |> List.map charToPlant
